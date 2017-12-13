6494d53153824c097d69d43b8da2e576f2a35b88
Chris Pitts <chris@thirstybear.co.uk>
2017-12-13 14:30:56 +0000

    Added simple self-migrating app

---
+++ src/main/java/uk/co/thirstybear/demos/flyway/SelfMigratingApp.java
@@ -0,0 +1,30 @@
+package uk.co.thirstybear.demos.flyway;
+
+import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
+import org.flywaydb.core.Flyway;
+
+import javax.sql.DataSource;
+
+public class SelfMigratingApp {
+    public static void main(String[] args) {
+        migrateDatabase();
+
+        // Now run the app...
+    }
+
+    private static void migrateDatabase() {
+        final Flyway flyway = new Flyway();
+        flyway.setSchemas("testSchema");
+        flyway.setDataSource(buildDataSource());
+        flyway.migrate();
+    }
+
+    private static DataSource buildDataSource() {
+        final MysqlDataSource dataSource = new MysqlDataSource();
+        dataSource.setURL("jdbc:mysql://localhost:3306");
+        dataSource.setDatabaseName("testSchema");
+        dataSource.setUser("root");
+        dataSource.setPassword("password");
+        return dataSource;
+    }
+}
